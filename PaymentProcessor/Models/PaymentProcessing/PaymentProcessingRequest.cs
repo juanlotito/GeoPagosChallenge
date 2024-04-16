3c7bac98f3f7597dbf83d6f68b821dfbaace3946
JLotito <jlotito@nosis.com>
2024-04-16 15:52:20 -0300

    refactor: added models into folder

--- PaymentProcessor/Models/PaymentProcessing/PaymentProcessingRequest.cs
+++ PaymentProcessor/Models/PaymentProcessing/PaymentProcessingRequest.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
 
-namespace PaymentProcessor.Models
+namespace PaymentProcessor.Models.PaymentProcessing
 {
     public class PaymentProcessingRequest
     {
