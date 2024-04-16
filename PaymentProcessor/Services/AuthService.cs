cf567d4e23c302eb000981c65889079f5c0bd3e1
JLotito <jlotito@nosis.com>
2024-04-16 14:12:32 -0300

    feature: add service implementations for auth and payment processor

---
+++ PaymentProcessor/Services/AuthService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
+using PaymentProcessor.Models.Auth;
+using PaymentProcessor.Services.Interface;
+
+namespace PaymentProcessor.Services
+{
+    public class AuthService : IAuthService
+    {
+        public string GenerateJwtToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("4c5nNI0zNIgBonQ4c5nNI0zNIgBonQ12"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "PublicApiIssuer",
+                audience: "PaymentProcessorAudience",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        public bool IsValidUser(UserCredentials credentials)
+        {
+            //TODO: Implementar metodo
+            return true;
+        }
+
+    }
+}
---
+++ PaymentProcessor/Services/PaymentProcessorService.cs
@@ -0,0 +1,33 @@
+﻿using PaymentProcessor.Models;
+using PaymentProcessor.Services.Interface;
+using System;
+
+namespace PaymentProcessor.Services
+{
+    public class PaymentProcessorService : IPaymentProcessorService
+    {
+        public PaymentProcessingResponse ProcessPayment(PaymentProcessingRequest request)
+        {
+            bool isAmountInteger = request.Amount == Math.Floor(request.Amount);
+
+            if (isAmountInteger)
+            {
+                return new PaymentProcessingResponse
+                {
+                    PaymentRequestId = request.PaymentRequestId,
+                    IsApproved = true,
+                    Message = "Payment is approved."
+                };
+            }
+            else
+            {
+                return new PaymentProcessingResponse
+                {
+                    PaymentRequestId = request.PaymentRequestId,
+                    IsApproved = false,
+                    Message = "Payment is denied. Amount must be an integer."
+                };
+            }
+        }
+    }
+}
