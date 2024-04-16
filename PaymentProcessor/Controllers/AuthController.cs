828eb148329f68cce313c73c4ffea5e656fe098f
JLotito <jlotito@nosis.com>
2024-04-16 14:10:30 -0300

    feature: add authorization controller

---
+++ PaymentProcessor/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentProcessor.Models.Auth;
+using PaymentProcessor.Services.Interface;
+
+namespace PaymentProcessor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserCredentials credentials)
+        {
+            if (_authService.IsValidUser(credentials))
+            {
+                var token = _authService.GenerateJwtToken(credentials.Username);
+                return Ok(new { Token = token });
+            }
+            return Unauthorized();
+        }
+
+    }
+}
