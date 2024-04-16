0407b77ae0a1f212611cedbabce6269b637572a8
JLotito <jlotito@nosis.com>
2024-04-16 14:10:44 -0300

    feature: add PaymentProcessorController

---
+++ PaymentProcessor/Controllers/PaymentProcessorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PaymentProcessor.Models;
+using PaymentProcessor.Services.Interface;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentProcessor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentProcessorController : ControllerBase
+    {
+        private readonly IPaymentProcessorService _paymentProcessorService;
+
+        public PaymentProcessorController(IPaymentProcessorService paymentProcessorService)
+        {
+            _paymentProcessorService = paymentProcessorService;
+        }
+
+        [HttpPost("process")]
+        [Authorize]
+        public ActionResult<PaymentProcessingResponse> ProcessPayment([FromBody, Required] PaymentProcessingRequest request)
+        {
+            try 
+            {
+                var response = _paymentProcessorService.ProcessPayment(request);
+                return Ok(response);
+            } 
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
