62148f996afd8d773a6004b64ba260665309a069
JLotito <jlotito@nosis.com>
2024-04-16 14:15:44 -0300

    feature: add queue utils

---
+++ PublicApi/Utils/QueuedHostedService.cs
@@ -0,0 +1,24 @@
+﻿using PublicApi.Services.Interface;
+
+namespace PublicApi.Utils
+{
+    public class QueuedHostedService : BackgroundService
+    {
+        private readonly IBackgroundTaskQueue _taskQueue;
+
+        public QueuedHostedService(IBackgroundTaskQueue taskQueue)
+        {
+            _taskQueue = taskQueue;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var workItem = await _taskQueue.DequeueAsync(stoppingToken);
+
+                await workItem(stoppingToken);
+            }
+        }
+    }
+}
