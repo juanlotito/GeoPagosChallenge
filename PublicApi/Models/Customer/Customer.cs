ed329b45e5f5ce6f34f7bbe32a5366544452ff4a
JLotito <jlotito@nosis.com>
2024-04-16 14:13:56 -0300

    feature: add customer base model

---
+++ PublicApi/Models/Customer/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace PublicApi.Models.Customer
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public int CustomerTypeId { get; set; }
+        public string CustomerTypeDescription { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
