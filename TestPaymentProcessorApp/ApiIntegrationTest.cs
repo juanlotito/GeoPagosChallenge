6046413cc97141fd42ba153bd09ec851d16941a0
JLotito <jlotito@nosis.com>
2024-04-16 16:51:13 -0300

    feature: add integration tests

---
+++ TestPaymentProcessorApp/ApiIntegrationTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+
+
+namespace TestPaymentProcessorApp
+{
+    public class ApiIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public ApiIntegrationTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetPaymentStatus_ReturnsNotFound_ForInvalidId()
+        {
+            // Arrange
+            var invalidId = 999;
+
+            // Act
+            var response = await _client.GetAsync($"/payments/status/{invalidId}");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetPaymentStatus_ReturnsOk_ForValidId()
+        {
+            // Arrange
+            var validId = 1;
+
+            // Act
+            var response = await _client.GetAsync($"/payments/status/{validId}");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
