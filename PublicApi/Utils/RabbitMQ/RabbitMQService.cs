f453f51128e6aa70f88597764bcf6db430f40b52
JLotito <jlotito@nosis.com>
2024-04-19 09:47:40 -0300

    refactor: db logic in rabbit service

--- PublicApi/Utils/RabbitMQ/RabbitMQService.cs
+++ PublicApi/Utils/RabbitMQ/RabbitMQService.cs
@@ -7,15 +7,16 @@ public class RabbitMQService : IRabbitMQService
 {
     private readonly ConnectionFactory _factory;
     private readonly IConnection _connection;
+    private readonly IConfiguration _configuration;
     private readonly IModel _channel;
     private bool _queueDeclared = false;
     private readonly IDbConnection _db;
-    public RabbitMQService(IDbConnection db)
+    public RabbitMQService(IConfiguration configuration)
     {
         _factory = new ConnectionFactory() { HostName = "rabbitmq" };
         _connection = _factory.CreateConnection();
         _channel = _connection.CreateModel();
-        _db = db;
+        _configuration = configuration;
     }
 
     public void SendMessage(string queueName, byte[] messageBody)
@@ -23,7 +24,7 @@ public class RabbitMQService : IRabbitMQService
         if (!_queueDeclared)
         {
             _channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
-            var consumer = new RabbitMQConsumer("payment_confirmation", new PaymentRepository(_db));
+            var consumer = new RabbitMQConsumer("payment_confirmation", new PaymentRepository(_configuration));
             _queueDeclared = true;
         }
 
